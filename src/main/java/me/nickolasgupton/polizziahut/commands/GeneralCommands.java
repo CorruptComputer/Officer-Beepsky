a85793033c34b790661a880ac7cd59a86b14325d
Nickolas Gupton <ngupton98@gmail.com>
2017-08-19 15:15:14 -0500

    Remove old GrumpyCode stuff Finally make a final decision on the LICENSE

--- src/main/java/me/nickolasgupton/polizziahut/commands/GeneralCommands.java
+++ src/main/java/me/nickolasgupton/polizziahut/commands/GeneralCommands.java
@@ -14,29 +14,21 @@ public class GeneralCommands {
 
         generalCommands.put("help", (event, args) -> {
             EmbedBuilder builder = new EmbedBuilder();
-            builder.withAuthorName("Grumpy Code");
-            builder.withAuthorIcon("http://i.imgur.com/1lWPIgn.png");
-            builder.withAuthorUrl("https://github.com/GrumpyCode");
-
             builder.withColor(255, 0, 0);
             builder.withTitle("__Command List__");
             builder.withDescription(
                     "**General Commands:**\n" +
-                    "`" + BotUtils.DEFAULT_PREFIX + "help` - You should already know this one.\n" +
-                    "`" + BotUtils.DEFAULT_PREFIX + "joinserver` - Bot will send you an invite to the Grumpy Code Discord server.\n\n"+
+                    "`" + BotUtils.DEFAULT_PREFIX + "help` - You should already know this one.\n\n" +
                     "**Music Commands:**\n" +
                     "`" + BotUtils.MUSIC_PREFIX + "queue <YouTube/SoundCloud URL>` - Bot will play the song at that link.\n" +
                     "`" + BotUtils.MUSIC_PREFIX + "stop` - Bot will clear the queue and leave the voice channel.\n" +
                     "`" + BotUtils.MUSIC_PREFIX + "skip` - Bot will skip the currently playing song.\n\n");
 
+            builder.withFooterText("PolizziaHut is an open source Discord bot, you can view the source here on [GitHub](https://github.com/CorruptComputer/PolizziaHut).");
+
             RequestBuffer.request(() -> event.getAuthor().getOrCreatePMChannel().sendMessage(builder.build()));
             event.getMessage().delete();
 
-        });
-
-        generalCommands.put("joinserver", (event, args) -> {
-            RequestBuffer.request(() -> event.getAuthor().getOrCreatePMChannel().sendMessage("https://discord.gg/pa9bssf"));
-            event.getMessage().delete();
         });
 
         return generalCommands;
