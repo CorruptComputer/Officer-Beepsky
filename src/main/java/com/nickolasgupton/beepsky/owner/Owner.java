3f1ce5b468f6e1f03588a21427727ac49892d96a
Nickolas Gupton <NGupton98@gmail.com>
2018-01-17 18:43:12 -0600

    Force shutdown messages to send before bot shuts down

--- src/main/java/com/nickolasgupton/beepsky/owner/Owner.java
+++ src/main/java/com/nickolasgupton/beepsky/owner/Owner.java
@@ -2,18 +2,15 @@ package com.nickolasgupton.beepsky.owner;
 
 import com.nickolasgupton.beepsky.BotUtils;
 import sx.blah.discord.util.EmbedBuilder;
-import sx.blah.discord.util.RequestBuffer;
 
 public class Owner {
   public static long ID;
 
   public static void sendMessage(String message) {
-    RequestBuffer.request(
-        () -> BotUtils.CLIENT.getUserByID(ID).getOrCreatePMChannel().sendMessage(message));
+    BotUtils.CLIENT.getUserByID(ID).getOrCreatePMChannel().sendMessage(message);
   }
 
   public static void sendMessage(EmbedBuilder message) {
-    RequestBuffer.request(
-        () -> BotUtils.CLIENT.getUserByID(ID).getOrCreatePMChannel().sendMessage(message.build()));
+    BotUtils.CLIENT.getUserByID(ID).getOrCreatePMChannel().sendMessage(message.build());
   }
 }
